9e65207a2b11ca57d4dd0b76d427c5adcef24f48
Alex Farmer <farmale10@gmail.com>
2019-09-20 12:53:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,6 @@
 
-class Song 
-  
-  attr_reader :title, :artist
-  def initialize(title, artist)
-    @title = title 
-    @artist = artist 
-  end 
-  
-  
-  
-  
-  
-end 
+class Song
+  attr_accessor :title, :artist
+end
+
 
